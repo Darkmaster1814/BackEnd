8f18944e69f05c5a555293423096a8022ffb9f8f
VictorVelasco <dani_gt_10@hotmal.com>
2023-03-05 21:14:16 -0600

    Revisar el ProductManager-deleteProduct y updatePrduct ya que l ejecutar el programa se vuelve recursivo y hace un bucle

--- index.js
+++ index.js
@@ -1,57 +1,72 @@
-class ProductManager {
-    #products;//Arreglo de productos
-    static id = 0; //Declaracion de una variable statica y privada
-    constructor() {
-        this.#products = [];
-    } 
-    //Getter del array de productos
-    getProducts() 
-    {
-        return this.#products;
-    } 
-    //Method para revisar si alguno de los productos existe en el array return true si existe
-    productExist(code) {
-        let exist=this.#products.some((product) => {
-            return (product.code === code) 
-        });
-        return exist;
-    } 
-    //Añade un producto al array
-    addProduct = (product) => {
-        let valid = true;//Variable flag para validar si los campos se llenaron, si se llenaron es true si falta alguno o es undefined  es false
-        /* Validador de campos */
-        Object.values(product).forEach((field) => {
-            if (field === "" || field === undefined) {
-                valid = false;
-                console.log("ERROR:Rellene todos los campos")
-            }});
-        /*Push de objetos en caso de que sean validos  */
-        if (!this.productExist(product?.code) &&valid) {
-            this.#products.push({
-                id: ++ProductManager.id,
-                ...product});
-        } 
-        else {
-            console.log("ERROR:Asegurese que el codigo no se repita");
-        }
-    }
-    /* Obtener producto por ID */
-    getProductById = (id) => {
-        let productById = this.#products.find((product) => product.id === id);
-        if (productById === undefined) {
-            console.log("ERROR:Not Found");
-        } else {
-            return productById;
-        }
-    }
-}
-
+//Testing para el product manager
+import ProductManager from "./Managers/ProductManager.js";
 
 //Creamos la instancia
-let productos = new ProductManager();
-//Producto prueba
-let productoPrueba = {
-    title: "producto prueba",
+let productos = new ProductManager("./Files/products.json");
+//Obtenemos de forma asyncronica todos los metodos del Product Manager
+const test = async () => {
+    try {
+        /* =======================================================TESTING=============================================== */
+        //Producto prueba
+        let productoPrueba = {
+            title: "producto prueba",
+            description: "Este producto es una prueba",
+            price: 200,
+            thumbnail: "Sin imagen",
+            code: "abc123",
+            stock: 25,
+        };
+        //Obtener productos
+        console.log("Obteniendo productos",await productos.getProducts());
+        //Agregar un producto
+        await productos.addProduct(productoPrueba);
+        //Buscar por ID
+        console.log("El producto con ID:1 ",await productos.getProductById(1));
+        //Agregar el mismo producto
+        await productos.addProduct(productoPrueba);
+        console.log("Obteniendo productos",await productos.getProducts());
+        //Buscar por ID que no existe
+        console.log("El producto con ID:2 (que no existe) ",await productos.getProductById(2));
+        //Agregando un segundo producto
+        await productos.addProduct({
+            title: "producto2",
+            description: "Este producto es una prueba",
+            price: 200,
+            thumbnail: "Sin imagen",
+            code: "abc124",
+            stock: 25,});
+        console.log("Despues de agregar un producto", await productos.getProducts());
+        //Actualizando un producto que existe
+        await productos.updateProduct(1, {
+            title: "producto1 Actualizdo",
+            description: "Este producto es una prueba",
+            price: 200,
+            thumbnail: "Sin imagen",
+            code: "abc123",
+            stock: 25,});
+        console.log("Despues de actualizar el producto ID: 1", await productos.getProducts());
+        //Eliminando un producto
+        console.log("Despues de agregar un producto ID:1", await productos.deleteProduct(1));
+        productos.deleteProduct(2);
+    } catch (error) {
+        console.log("ERROR:", error); 
+    }
+};
+
+function main(){
+test();
+}
+main();
+
+
+
+
+/* 
+
+
+console.log("Actualizando un producto");
+productoPrueba = {
+    title: "producto1 Actualizado",
     description: "Este producto es una prueba",
     price: 200,
     thumbnail: "Sin imagen",
@@ -59,13 +74,7 @@ let productoPrueba = {
     stock: 25,
 };
 
-/* =======================================================TESTING=============================================== */
-console.log("El arreglo vacio:", productos.getProducts());
-productos.addProduct(productoPrueba);
-console.log("Despues de agregar un producto",productos.getProducts());
-console.log("si se agrega el producto otra vez");
-productos.addProduct(productoPrueba);
-console.log(`Buscando un producto con ID: 1`);
-console.log(productos.getProductById(1));
-console.log("Buscando un Id que no existe");
-console.log(productos.getProductById(111));
+console.log(productos.getProducts());
+console.log("Eliminando un producto");
+productos.deleteProduct(2);
+console.log(productos.getProducts()); */
